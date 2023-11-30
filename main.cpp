6a824d90ddc1a304895f4b4ad6cbc39b75890d9d
Johny <94753989+Joneky@users.noreply.github.com>
2023-11-30 12:57:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
-#include <fstream>
 #include <iostream>
-#include <string>
-using namespace std;
-int x,y;
-int main()
-{
-    ofstream prout("text2.txt");
-    ifstream print("text.txt");
-    print >> x >> y;
-    prout << x * y <<endl;
-};
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void Doit( double x, double y){
+    if (x >= -3 && x < -2) {
+        y = -2 - x;
+        cout << "Функция равна: " << y;
+    } else if (x >= -2 && x < -1) {
+        y = sqrt(1 - (x + 1) * (x + 1));
+        cout << "Функция равна: " << y;
+    } else if (x >= -1 && x < 1) {
+        y = 1;
+        cout << "Функция равна: " << -y;
+    } else if (x >= 1 && x < 2) {
+        y = -x + 1;
+        cout << "Функция равна: " << y;
+    } else if (x >= 2 && x <= 5) {
+        y = -1;
+        cout << "Функция равна: " << y;
+    } else {
+        cout << "x вне диапазона функции" << endl;
+    }
+}
+int main() {
+    double x, y;
+    cout << "Введите значение x: ";
+    cin >> x;
+    Doit(x,y);
+    return 0;
+}
